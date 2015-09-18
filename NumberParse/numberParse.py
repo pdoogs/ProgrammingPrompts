8f975eda17644b704756eb40908e57c1847ef1bb
pdoogs <patrick.c.dugan@gmail.com>
2015-09-18 16:38:38 -0400

    First commit of solution to a /r/ProgrammingPrompt on reddit

---
+++ NumberParse/numberParse.py
@@ -0,0 +1,58 @@
+import sys
+
+digits = { 'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9 }
+teens = { 'ten': 10, 'eleven': 11, 'twelve': 12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15, 'sixteen': 16, 'seventeen': 17, 'eighteen': 18, 'nineteen': 19}
+tens = { 'twenty': 20, 'thirty': 30, 'fourty': 40, 'fifty': 50, 'sixty':60, 'seventy':70, 'eighty':80, 'ninety':90 }
+multipliers = { 'hundred':100, 'thousand':1000, 'million':1000000, 'billion': 1000000000 }
+
+def read_tokens():
+	tokens = []
+	if (len(sys.argv) > 1):
+		for i in range(1,len(sys.argv)):
+			tokens.append(sys.argv[i])
+	else:
+		strNum = input('Type a number ("One thousand eighty two") ')
+		tokens = strNum.lower().split()
+	return tokens
+
+def parse_teens(digitToken, tokens, intVal):
+	intVal += tokens[digitToken]
+	tokens = []
+	return intVal
+
+def parse_digit(digitToken, tokens, intVal):
+	intVal += digits[digitToken]
+
+	if (len(tokens) == 0): return intVal
+
+	tok = tokens.pop(0)
+	if (tok in teens):
+		intVal *= 100
+		parse_teens(tok, tokens, intVal)
+	elif (tok in tens):
+		return intVal
+	elif (tok in multipliers):
+		return intVal
+	else:
+		return intVal
+
+	return intVal
+
+intVal = 0
+tokens = read_tokens()
+
+while len(tokens) > 0:
+	tok = tokens.pop(0)
+	print(digits)
+	print(tok in digits)
+	print(tok)
+	if (tok == 'zero'):
+		#intVal = 0
+		break
+	elif (tok in digits):
+		print(intVal)
+		intVal = parse_digit(tok, tokens, intVal)
+		print(intVal)
+		
+
+#print(intVal)
